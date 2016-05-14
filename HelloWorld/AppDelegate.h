0107f158a2566fe921d1652baa3e49d24721ab31
AppsWorkforce 4 <appsworkforce4@appsworkforces-imac-4.local>
2016-05-14 19:42:39 +0100

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by AppsWorkforce 4 on 14/05/2016.
+//  Copyright © 2016 Ehbraheem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by AppsWorkforce 4 on 14/05/2016.
+//  Copyright © 2016 Ehbraheem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
